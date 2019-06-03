811d05cf143a149c089a0b01fec95f1f417034a1
Meitar Cohen <meitarc@gmail.com>
2019-06-03 19:25:38 +0300

    add function that find images in the same GPS area

--- Common_Image_Part_A/GPS/readGpsLocation.py
+++ Common_Image_Part_A/GPS/readGpsLocation.py
@@ -1,12 +1,29 @@
 import exifread
 imgArray={}
+sameGpsPics=[]
+def first(img):
+    tags = exifread.process_file(open(img, 'rb'))
+    geo = {i:tags[i] for i in tags.keys() if i=='GPS GPSLatitude' or i=='GPS GPSLongitude' or i=='GPS GPSAltitude'}
+    imgArray["first"]=geo
+
 def addImag(img):
     tags = exifread.process_file(open(img, 'rb'))
     geo = {i:tags[i] for i in tags.keys() if i=='GPS GPSLatitude' or i=='GPS GPSLongitude' or i=='GPS GPSAltitude'}
-
     imgArray[img]=geo
 
-addImag('0.jpg')
-addImag('expImg.jpg')
+first('pics_for_gps/20190603_172000.jpg')
 
-print(imgArray)
+import os
+for filename in os.listdir('pics_for_gps'):
+    addImag("pics_for_gps/"+filename)
+
+for key, value in zip(imgArray,imgArray.values()):
+    #print(imgArray[key].values())
+    #print(imgArray["first"].values())
+    if str(imgArray[key].values()) == str(imgArray["first"].values()):
+        sameGpsPics.append(key)
+        print("true")
+    else:
+        print("false")
+
+print(sameGpsPics)
