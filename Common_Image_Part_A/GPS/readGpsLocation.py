5501c96397ae4e12f76d01743fd5486fa7a850b8
Meitar Cohen <meitarc@gmail.com>
2019-06-04 12:29:33 +0300

    finish function that find images in the same GPS area

--- Common_Image_Part_A/GPS/readGpsLocation.py
+++ Common_Image_Part_A/GPS/readGpsLocation.py
@@ -1,29 +1,30 @@
-import exifread
-imgArray={}
-sameGpsPics=[]
-def first(img):
-    tags = exifread.process_file(open(img, 'rb'))
-    geo = {i:tags[i] for i in tags.keys() if i=='GPS GPSLatitude' or i=='GPS GPSLongitude' or i=='GPS GPSAltitude'}
-    imgArray["first"]=geo
+def find_imgs_same_gps(imgSource, folderPath):
+    import exifread
+    firstImg={}
+    imgArray={}
+    sameGpsPics=[]
+    def first(img):
+        tags = exifread.process_file(open(img, 'rb'))
+        geo = {i:tags[i] for i in tags.keys() if i=='GPS GPSLatitude' or i=='GPS GPSLongitude' or i=='GPS GPSAltitude'}
+        firstImg["first"]=geo
 
-def addImag(img):
-    tags = exifread.process_file(open(img, 'rb'))
-    geo = {i:tags[i] for i in tags.keys() if i=='GPS GPSLatitude' or i=='GPS GPSLongitude' or i=='GPS GPSAltitude'}
-    imgArray[img]=geo
+    def addImag(img):
+        tags = exifread.process_file(open(img, 'rb'))
+        geo = {i:tags[i] for i in tags.keys() if i=='GPS GPSLatitude' or i=='GPS GPSLongitude' or i=='GPS GPSAltitude'}
+        imgArray[img]=geo
 
-first('pics_for_gps/20190603_172000.jpg')
+    first(imgSource)
 
-import os
-for filename in os.listdir('pics_for_gps'):
-    addImag("pics_for_gps/"+filename)
+    import os
+    for filename in os.listdir(folderPath):
+        addImag(folderPath+"/"+filename)
 
-for key, value in zip(imgArray,imgArray.values()):
-    #print(imgArray[key].values())
-    #print(imgArray["first"].values())
-    if str(imgArray[key].values()) == str(imgArray["first"].values()):
-        sameGpsPics.append(key)
-        print("true")
-    else:
-        print("false")
+    for key, value in zip(imgArray,imgArray.values()):
+        if str(imgArray[key].values()) == str(firstImg["first"].values()):
+            sameGpsPics.append(key)
 
-print(sameGpsPics)
+    return sameGpsPics
+
+
+myImgArray=find_imgs_same_gps("pics_for_gps/20190603_172000.jpg","pics_for_gps")
+print(myImgArray)
